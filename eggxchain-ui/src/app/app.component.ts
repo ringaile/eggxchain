a7094dfa013dfff54968aa944998d07b9abb47aa
Marko Martin <markomartin@gmx.net>
2018-10-06 18:57:58 +0200

    UI.

--- eggxchain-ui/src/app/app.component.ts
+++ eggxchain-ui/src/app/app.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-import * as Web3 from 'web3';
+//import * as Web3 from 'web3';
+import * as TruffleContract from 'truffle-contract';
+
+declare let require: any;
+const Web3 = require('web3');
+
+declare let window: any;
+
+const artifacts = require('../../../build/contracts/SupplyChainList.json');
 
 @Component({
   selector: 'app-root',
@@ -10,7 +19,25 @@ import * as Web3 from 'web3';
 export class AppComponent {
   result: string;
 
-  constructor() {
-    let web3 = new Web3.providers.HttpProvider("http://localhost:7545");
+  constructor(private httpClient: HttpClient) {
+    window.addEventListener('load', (event) => {
+      this.bootstrapWeb3();
+    });
+  }
+
+  private bootstrapWeb3(): void {
+    //let web3Provider = (window.web3 && window.web3.currentProvider) || new Web3.providers.HttpProvider("http://localhost:7545");
+    let web3Provider = window.web3.currentProvider;
+    let web3 = new Web3(web3Provider);
+    web3.eth.getAccounts((err, accounts) => {
+      this.result = accounts.length;
+    });
+    //this.httpClient.get("assets/SupplyChainList.json").subscribe(artifacts => {
+      let contract = TruffleContract(artifacts);
+      contract.setProvider(web3Provider);
+      contract.deployed().then(instance => {
+        instance.createEggBox(1, 2, 3);
+      });
+    //});
   }
 }
