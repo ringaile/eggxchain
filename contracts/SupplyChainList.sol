f1e06fa01288e018b75923d08c56933a5a51c125
cyen <cyen@ad.uni-mannheim.de>
2018-10-06 15:52:21 +0200

    remove warning and unnecessary contracts

--- contracts/SupplyChainList.sol
+++ contracts/SupplyChainList.sol
@@ -1,41 +1,54 @@
 pragma solidity ^0.4.18;
 
 contract SupplyChainList {
+  // state variable
+  address seller;
+  string name;
+  string description;
+  uint256 price;
+  address contractOwner;
+  uint eggBoxCounter;
 
-    struct EggBox {
-        uint id;
-        uint barcode;
-        address farmer;
-        uint timestamp;
-        uint longitude;
-        uint latitude;
+  struct EggBox {
+      uint id;
+      uint barcode;
+      address farmer;
+      uint timestamp;
+      uint longitude;
+      uint latitude;
+  }
+
+  mapping (uint => EggBox) public eggBoxes;
+
+  //constructor
+  constructor() public {
+      contractOwner = msg.sender;
+      eggBoxCounter = 0;
+  }
+
+  //
+  function createEggBox(uint _barcode, uint _longitude, uint _latitude) public{
+      eggBoxes[eggBoxCounter] = EggBox(
+          eggBoxCounter,
+          _barcode,
+          msg.sender,
+          now,
+          _longitude,
+          _latitude
+      );
+      eggBoxCounter++;
     }
 
-    mapping (uint => EggBox) public eggBoxes;
-    uint eggBoxCounter;
 
-    address contractOwner;
-
-    function SupplyChainList() public {
-        contractOwner = msg.sender;
-        eggBoxCounter = 0;
-    }
-
-    function createEggBox(uint barcode, uint longitude, uint latitude) public{
-        eggBoxCounter++;
-        eggBoxes[eggBoxCounter] = EggBox(
-            eggBoxCounter,
-            barcode,
-            msg.sender,
-            now,
-            longitude,
-            latitude
-        );
-    }
-
-    function getEggBox(uint id) public returns (
-        uint barcode, address farmer, uint timestamp, uint longitude, uint latitude) {
+    function getEggBox(uint id) public view returns (
+        uint _id,
+        uint _barcode,
+        address _farmer,
+        uint _timestamp,
+        uint _longitude,
+        uint _latitude) {
             EggBox memory eggbox = eggBoxes[id];
-            return (eggbox.barcode, eggbox.farmer, eggbox.timestamp, eggbox.longitude, eggbox.latitude);
+            return (eggbox.id, eggbox.barcode, eggbox.farmer, eggbox.timestamp, eggbox.longitude, eggbox.latitude);
     }
+
 }
