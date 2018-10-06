a672928b662e8475ebc026b6120882553606439e
Marko Martin <markomartin@gmx.net>
2018-10-06 16:08:34 +0200

    Angular.

--- eggxchain-ui/src/app/app.component.ts
+++ eggxchain-ui/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import * as Web3 from 'web3';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'eggxchain-ui';
+  result: string;
+
+  constructor() {
+    let web3 = new Web3.providers.HttpProvider("http://localhost:7545");
+  }
 }
